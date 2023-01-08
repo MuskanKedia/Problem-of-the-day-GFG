b4ae2eaaf454b176cfba6dffd5aa62f0fc613c7d
Muskan Kedia <81368928+MuskanKedia@users.noreply.github.com>
2023-01-08 23:37:16 +0530

    Create Absolute difference divisible by K.java

---
+++ Absolute difference divisible by K.java
@@ -0,0 +1,18 @@
+class Solution {
+	static long countPairs(int n, int[] arr, int k) {
+		int count=0;
+		long[] rem=new long[k];
+        for(int i=0;i<n;i++)
+        {
+            rem[arr[i]%k]++;
+        }
+        for(int i=0;i<k;i++)
+        {
+            count+=((rem[i]-1)*rem[i])/2;
+        }
+        return count;
+	}
+}
+
+//Time Complexity : O(N)
+//Space Complexity : O(N)
