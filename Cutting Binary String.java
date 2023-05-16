42defbddd30a12034acdbf26abf284444ba472c4
Muskan Kedia <81368928+MuskanKedia@users.noreply.github.com>
2023-05-16 19:14:42 +0530

    POTD

---
+++ Cutting Binary String.java
@@ -0,0 +1,12 @@
+class Solution{
+    static int cuts(String str)
+    {
+        for (int i = 22; i >= 0; i--)
+			str = str.replaceAll(Long.toBinaryString((long) Math.pow(5, i)), "#");
+
+		return str.contains("0") ? -1 : str.length();
+    }
+}
+
+//Time Complexity : O(N)
+//Space Complexity : O(1)
